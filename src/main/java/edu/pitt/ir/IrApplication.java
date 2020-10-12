b7d48a2aef369afe68fa6da92eaf737d39389e68
Xuzijian <zix10@pitt.edu>
2020-10-12 18:07:07 -0400

    init backend project

---
+++ src/main/java/edu/pitt/ir/IrApplication.java
@@ -0,0 +1,13 @@
+package edu.pitt.ir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrApplication.class, args);
+    }
+
+}
